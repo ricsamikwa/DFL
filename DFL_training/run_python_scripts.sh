3555830709e5455d83b1ceb72309f0c11a918ff9
ricsamikwa <esamikwa@gmail.com>
2024-05-31 00:09:58 +0200

    server script del

--- DFL_training/run_python_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Call Python scripts in a specific order and run them in the background using nohup
-nohup python3 run_DFL_unit.py --split False --group True &
-
-# Return immediately after calling the Python scripts
-exit
